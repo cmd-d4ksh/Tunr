57d7a654cdc51ebe70eaddddd00d25fcbf812737
Daksh Shah <dakshonthego@gmail.com>
2026-01-27 22:50:28 +0530

    Initial Commit

---
+++ Tunr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tunr
+//
+//  Created by Daksh Shah on 27/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tunr/TunrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunrApp.swift
+//  Tunr
+//
+//  Created by Daksh Shah on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TunrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
